8ad6e3e4b433f77479d90a752a6768dbdd2f7d7c
eastar-DS <sien0104@gmail.com>
2025-05-02 13:31:12 +0900

    feat: Like 모델 생성

---
+++ lib/community/domain/model/like.dart
@@ -0,0 +1,11 @@
+// lib/community/domain/model/like.dart
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'like.freezed.dart';
+
+@freezed
+abstract class Like with _$Like {
+  const factory Like({
+    required String boardId,
+    required String memberId,
+  }) = _Like;
+}
