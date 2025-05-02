624970aced097d04284e03c1e51954587e185b07
eastar-DS <sien0104@gmail.com>
2025-05-02 14:44:37 +0900

    fix: postId 타입 스트링으로 변경

--- lib/community/presentation/community_list/community_list_action.dart
+++ lib/community/presentation/community_list/community_list_action.dart
@@ -1,3 +1,4 @@
+import 'package:devlink_mobile_app/community/module/util/%08community_tab_type_enum.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 part 'community_list_action.freezed.dart';
 
@@ -5,7 +6,7 @@ part 'community_list_action.freezed.dart';
 sealed class CommunityListAction with _$CommunityListAction {
   const factory CommunityListAction.refresh() = Refresh;
   const factory CommunityListAction.changeTab(CommunityTabType tab) = ChangeTab;
-  const factory CommunityListAction.tapPost(int postId) = TapPost;
+  const factory CommunityListAction.tapPost(String postId) = TapPost;
   const factory CommunityListAction.tapSearch() = TapSearch;
   const factory CommunityListAction.tapWrite() = TapWrite;
 }
