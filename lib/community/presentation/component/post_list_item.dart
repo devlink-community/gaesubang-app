cd4a0ac35cfe1e98ab44b654b7a6fa2b848452b0
eastar-DS <sien0104@gmail.com>
2025-05-02 13:59:59 +0900

    feat: PostListItem 컴포넌트 생성

---
+++ lib/community/presentation/component/post_list_item.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../../domain/model/post.dart';
+
+class PostListItem extends StatelessWidget {
+  const PostListItem({
+    super.key,
+    required this.post,
+    required this.onTap,
+  });
+
+  final Post post;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) => ListTile(
+        title: Text(post.title),
+        subtitle: Text('${post.member.nickname} · ${post.createdAt.toLocal()}'),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Icon(Icons.favorite_border, size: 16),
+            const SizedBox(width: 4),
+            Text('${post.like.length}'),
+          ],
+        ),
+        onTap: onTap,
+      );
+}
