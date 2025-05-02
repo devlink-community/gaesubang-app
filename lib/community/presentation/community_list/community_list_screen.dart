6c622e3441ac48eecd6c71ce95aa5acfa3686e1e
eastar-DS <sien0104@gmail.com>
2025-05-02 13:59:15 +0900

    feat: CommunityListScreen 생성

---
+++ lib/community/presentation/community_list/community_list_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'community_list_state.dart';
+import 'community_list_action.dart';
+import '../component/post_list_item.dart';
+
+class CommunityListScreen extends StatelessWidget {
+  const CommunityListScreen({
+    super.key,
+    required this.state,
+    required this.onAction,
+  });
+
+  final CommunityListState state;
+  final void Function(CommunityListAction action) onAction;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('커뮤니티'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () => onAction(const CommunityListAction.tapSearch()),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => onAction(const CommunityListAction.tapWrite()),
+          child: const Icon(Icons.edit),
+        ),
+        body: RefreshIndicator(
+          onRefresh: () async => onAction(const CommunityListAction.refresh()),
+          child: Column(
+            children: [
+              _buildTabBar(),
+              Expanded(child: _buildPostList()),
+            ],
+          ),
+        ),
+      );
+
+  Widget _buildTabBar() => TabBar(
+        labelColor: Colors.blue,
+        indicatorColor: Colors.blue,
+        unselectedLabelColor: Colors.grey,
+        onTap: (i) => onAction(
+          CommunityListAction.changeTab(
+            i == 0 ? CommunityTabType.popular : CommunityTabType.newest,
+          ),
+        ),
+        tabs: const [
+          Tab(text: '인기순'),
+          Tab(text: '최신순'),
+        ],
+      );
+
+  Widget _buildPostList() {
+    switch (state.postList) {
+      case AsyncLoading():
+        return const Center(child: CircularProgressIndicator());
+      case AsyncError(:final error, :_):
+        return Center(child: Text((error as Failure).message));
+      case AsyncData(:final value):
+        final list = value ?? [];
+        if (list.isEmpty) {
+          return const Center(child: Text('등록된 게시글이 없습니다'));
+        }
+        return ListView.builder(
+          physics: const AlwaysScrollableScrollPhysics(),
+          itemCount: list.length,
+          itemBuilder: (_, i) => PostListItem(
+            post: list[i],
+            onTap: () => onAction(CommunityListAction.tapPost(list[i].id)),
+          ),
+        );
+    }
+  }
+}
