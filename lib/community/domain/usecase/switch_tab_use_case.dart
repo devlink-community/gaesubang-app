091050bb10228d851a6aea3a83764762c4de07ea
eastar-DS <sien0104@gmail.com>
2025-05-02 13:32:10 +0900

    feat: SwitchTabUseCase 생성

---
+++ lib/community/domain/usecase/switch_tab_use_case.dart
@@ -0,0 +1,5 @@
+import '../../presentation/community_list/community_list_state.dart';
+
+class SwitchTabUseCase {
+  CommunityTabType toggle(CommunityTabType current, CommunityTabType next) => next;
+}
