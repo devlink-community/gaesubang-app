8aec0223efca1cf865d2b9cbf41a35163a02f186
eastar-DS <sien0104@gmail.com>
2025-05-07 11:37:34 +0900

    feat: PostListItem 컴포넌트 UI 수정

--- lib/community/presentation/component/post_list_item.dart
+++ lib/community/presentation/component/post_list_item.dart
@@ -1,29 +1,60 @@
 import 'package:devlink_mobile_app/community/domain/model/post.dart';
 import 'package:flutter/material.dart';
 
-
 class PostListItem extends StatelessWidget {
-  const PostListItem({
-    super.key,
-    required this.post,
-    required this.onTap,
-  });
+  const PostListItem({super.key, required this.post, required this.onTap});
 
   final Post post;
   final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) => ListTile(
-        title: Text(post.title),
-        subtitle: Text('${post.member.nickname} · ${post.createdAt.toLocal()}'),
-        trailing: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Icon(Icons.favorite_border, size: 16),
-            const SizedBox(width: 4),
-            Text('${post.like.length}'),
-          ],
+    leading: Image.network(
+      "https://i.namu.wiki/i/R0AhIJhNi8fkU2Al72pglkrT8QenAaCJd1as-d_iY6MC8nub1iI5VzIqzJlLa-1uzZm--TkB-KHFiT-P-t7bEg.webp",
+      width: 70,
+      height: 70,
+      fit: BoxFit.cover,
+      loadingBuilder: (context, child, loadingProgress) {
+        if (loadingProgress == null) return child;
+        return const Center(child: CircularProgressIndicator());
+      },
+    ),
+    // title: Text(post.title),
+    title: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '${post.createdAt.toLocal().year}-${post.createdAt.toLocal().month.toString().padLeft(2, '0')}-${post.createdAt.toLocal().day.toString().padLeft(2, '0')} · ${post.member.nickname}',
+          style: TextStyle(fontSize: 12),
         ),
-        onTap: onTap,
-      );
+        Text(post.title),
+      ],
+    ),
+    subtitle: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          post.hashTag.map((e) => e.content).join(' '),
+          style: TextStyle(fontSize: 12),
+        ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Icon(Icons.comment, size: 16),
+              const SizedBox(width: 4),
+              Text('${post.comment.length}'),
+              const SizedBox(width: 8),
+              const Icon(Icons.favorite_border, size: 16),
+              const SizedBox(width: 4),
+              Text('${post.like.length}'),
+            ],
+          ),
+        ),
+      ],
+    ),
+
+    onTap: onTap,
+  );
 }
