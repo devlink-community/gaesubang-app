876b3570eb547cb6887de1d5fdd55c81cc0154a5
KangJiWon <doggodking@gmail.com>
2025-05-07 15:06:53 +0900

    fix: TimerCircleProgress 디자인 수정 - 피그마 기준으로 초안 작성

--- lib/group/presentation/group_timer/components/timer_circle_progress.dart
+++ lib/group/presentation/group_timer/components/timer_circle_progress.dart
@@ -1,6 +1,74 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+
+class TimerCircleProgress extends StatelessWidget {
+  const TimerCircleProgress({
+    super.key,
+    required this.elapsedSeconds,
+    required this.isRunning,
+    this.onTap,
+  });
+
+  final int elapsedSeconds;
+  final bool isRunning;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    // 시간을 시:분:초 형식으로 변환
+    final hours = elapsedSeconds ~/ 3600;
+    final minutes = (elapsedSeconds % 3600) ~/ 60;
+    final seconds = elapsedSeconds % 60;
+
+    final timeText =
+        '${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
+
+    return Container(
+      width: double.infinity,
+      height: 120,
+      decoration: BoxDecoration(
+        color: const Color(0xFFE6E6FA), // 연한 보라색 배경
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          // 타이머 텍스트
+          Text(
+            timeText,
+            style: const TextStyle(
+              fontSize: 42,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+
+          // 재생 버튼
+          Positioned(
+            bottom: 10,
+            child: InkWell(
+              onTap: onTap,
+              child: Container(
+                width: 36,
+                height: 36,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF8080FF), // 보라색 배경
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(
+                  isRunning ? Icons.pause : Icons.play_arrow,
+                  color: Colors.white,
+                  size: 20,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/*
 
 class TimerCircleProgress extends StatelessWidget {
   const TimerCircleProgress({
@@ -95,3 +163,4 @@ class _TimerCirclePainter extends CustomPainter {
     return oldDelegate.progress != progress;
   }
 }
+*/
