a0d74775bb0607b44457f053619bea70b8f649e2
KangJiWon <doggodking@gmail.com>
2025-05-07 15:50:45 +0900

    tset: 그룹 타이머 내에서 다른 화면 이동 테스트

--- lib/group/module/group_timer_test.dart
+++ lib/group/module/group_timer_test.dart
@@ -1,5 +1,7 @@
 import 'package:devlink_mobile_app/group/presentation/group_timer/group_timer_screen_root.dart';
+import 'package:devlink_mobile_app/group/presentation/group_timer/mock_screen/mock_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 /// 그룹 타이머 테스트용 메인 파일
@@ -9,25 +11,48 @@ void main() {
   // 테스트용 그룹 ID
   const testGroupId = 'group_0';
 
+  // 테스트용 라우터 설정
+  final router = GoRouter(
+    initialLocation: '/group/$testGroupId',
+    routes: [
+      GoRoute(
+        path: '/group/:id',
+        builder:
+            (context, state) =>
+                GroupTimerScreenRoot(groupId: state.pathParameters['id']!),
+      ),
+      GoRoute(
+        path: '/group/:id/attendance',
+        builder:
+            (context, state) =>
+                MockGroupAttendanceScreen(groupId: state.pathParameters['id']!),
+      ),
+      GoRoute(
+        path: '/group/:id/settings',
+        builder:
+            (context, state) =>
+                MockGroupSettingsScreen(groupId: state.pathParameters['id']!),
+      ),
+      GoRoute(
+        path: '/user/:id/profile',
+        builder:
+            (context, state) =>
+                MockUserProfileScreen(userId: state.pathParameters['id']!),
+      ),
+    ],
+    errorBuilder:
+        (context, state) => Scaffold(
+          body: Center(child: Text('경로를 찾을 수 없습니다: ${state.fullPath}')),
+        ),
+  );
+
   runApp(
     ProviderScope(
-      child: MaterialApp(
+      child: MaterialApp.router(
         title: '그룹 타이머 테스트',
         theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
-        home: const GroupTimerTestScreen(groupId: testGroupId),
+        routerConfig: router,
       ),
     ),
   );
-}
-
-/// 그룹 타이머 테스트 화면 래퍼
-class GroupTimerTestScreen extends StatelessWidget {
-  final String groupId;
-
-  const GroupTimerTestScreen({super.key, required this.groupId});
-
-  @override
-  Widget build(BuildContext context) {
-    return GroupTimerScreenRoot(groupId: groupId);
-  }
 }
