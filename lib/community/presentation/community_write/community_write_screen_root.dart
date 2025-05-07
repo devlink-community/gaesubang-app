6b6a6126d33afb8125e32da8a39dd52b4ca3763d
eastar-DS <sien0104@gmail.com>
2025-05-07 13:42:51 +0900

    Create community_write_screen_root.dart

---
+++ lib/community/presentation/community_write/community_write_screen_root.dart
@@ -0,0 +1,20 @@
+// lib/community/presentation/community_write/community_write_screen_root.dart
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:devlink_mobile_app/community/presentation/community_write/community_write_notifier.dart';
+import 'package:devlink_mobile_app/community/presentation/community_write/community_write_screen.dart';
+
+class CommunityWriteScreenRoot extends ConsumerWidget {
+  const CommunityWriteScreenRoot({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state    = ref.watch(communityWriteNotifierProvider);
+    final notifier = ref.read(communityWriteNotifierProvider.notifier);
+
+    return CommunityWriteScreen(
+      state: state,
+      onAction: notifier.onAction,
+    );
+  }
+}
