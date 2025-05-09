08409ddcc6bb9a275eaed62147f2abdc16c1e052
Hwamok Lee <ghkahr1890@gmail.com>
2025-05-09 16:23:31 +0900

    fix(profile): intro screen 변경

--- lib/edit_intro/presentation/screens/edit_intro_screen.dart
+++ lib/edit_intro/presentation/screens/edit_intro_screen.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:devlink_mobile_app/edit_intro/presentation/providers/edit_intro_provider.dart';
+
+import '../../module/edit_intro_di.dart';
 
 class EditIntroScreen extends ConsumerStatefulWidget {
   const EditIntroScreen({super.key});
