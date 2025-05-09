251326ad82beab6ed9f5ba6a4064c5c1be13a35b
eastar-DS <dororo0104@naver.com>
2025-05-09 09:06:36 +0900

    refactor(community): CreateCommentUseCase가 Result<List<Comment>>를 AsyncValue<List<Comment>>로 변환하도록 수정

--- lib/community/domain/usecase/create_comment_use_case.dart
+++ lib/community/domain/usecase/create_comment_use_case.dart
@@ -1,15 +1,30 @@
+// lib/community/domain/usecase/create_comment_use_case.dart
+
 import 'package:devlink_mobile_app/community/domain/model/comment.dart';
 import 'package:devlink_mobile_app/community/domain/repository/post_repository.dart';
 import 'package:devlink_mobile_app/core/result/result.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class CreateCommentUseCase {
   const CreateCommentUseCase({required PostRepository repo}) : _repo = repo;
   final PostRepository _repo;
 
-  Future<Result<List<Comment>>> execute({
+  Future<AsyncValue<List<Comment>>> execute({
     required String postId,
     required String memberId,
     required String content,
-  }) =>
-      _repo.createComment(postId: postId, memberId: memberId, content: content);
+  }) async {
+    final result = await _repo.createComment(
+      postId: postId, 
+      memberId: memberId, 
+      content: content
+    );
+    
+    switch (result) {
+      case Success(:final data):
+        return AsyncData(data);
+      case Error(failure: final failure):
+        return AsyncError(failure, failure.stackTrace ?? StackTrace.current);
+    }
+  }
 }
