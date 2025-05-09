210f7a43047ab4606d44a72252efb83f0d7701ab
Kein Moon <successhelper2000@gmail.com>
2025-05-09 23:56:08 +0900

    refactor: 비밀번호 재설정 기능 초기 구현
              상태(ForgotPasswordState) 정의: 이메일, 오류 메시지, 비밀번호 재설정 결과 상태 관리
              액션(ForgotPasswordAction) 정의: 이메일 변경, 포커스 변경, 재설정 이메일 전송, 로그인 화면 이동

--- lib/auth/presentation/forget_password/forget_password_action.dart
+++ lib/auth/presentation/forget_password/forget_password_action.dart
@@ -1,19 +1,12 @@
-// lib/auth/presentation/forget_password/forget_password_action.dart
+// lib/auth/presentation/forgot_password/forgot_password_action.dart
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-part 'forget_password_action.freezed.dart';
+part 'forgot_password_action.freezed.dart';
 
 @freezed
-sealed class ForgetPasswordAction with _$ForgetPasswordAction {
-  // 이메일 필드 변경
-  const factory ForgetPasswordAction.emailChanged(String email) = EmailChanged;
-
-  // 이메일 포커스 변경 (유효성 검증 트리거)
-  const factory ForgetPasswordAction.emailFocusChanged(bool hasFocus) = EmailFocusChanged;
-
-  // 비밀번호 재설정 요청 제출
-  const factory ForgetPasswordAction.submit() = Submit;
-
-  // 로그인 화면으로 돌아가기
-  const factory ForgetPasswordAction.navigateToLogin() = NavigateToLogin;
+sealed class ForgotPasswordAction with _$ForgotPasswordAction {
+  const factory ForgotPasswordAction.emailChanged(String email) = EmailChanged;
+  const factory ForgotPasswordAction.emailFocusChanged(bool hasFocus) = EmailFocusChanged;
+  const factory ForgotPasswordAction.sendResetEmail() = SendResetEmail;
+  const factory ForgotPasswordAction.navigateToLogin() = NavigateToLogin;
 }
