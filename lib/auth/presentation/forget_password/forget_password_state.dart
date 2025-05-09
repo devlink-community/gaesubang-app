393eb9ce4c3aff37d913877a54b94168cbd5019e
Kein Moon <successhelper2000@gmail.com>
2025-05-09 23:56:08 +0900

    refactor: 비밀번호 재설정 기능 초기 구현
              상태(ForgotPasswordState) 정의: 이메일, 오류 메시지, 비밀번호 재설정 결과 상태 관리
              액션(ForgotPasswordAction) 정의: 이메일 변경, 포커스 변경, 재설정 이메일 전송, 로그인 화면 이동

--- lib/auth/presentation/forget_password/forget_password_state.dart
+++ lib/auth/presentation/forget_password/forget_password_state.dart
@@ -1,27 +1,18 @@
-// lib/auth/presentation/forget_password/forget_password_state.dart
+// lib/auth/presentation/forgot_password/forgot_password_state.dart
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-part 'forget_password_state.freezed.dart';
+part 'forgot_password_state.freezed.dart';
 
 @freezed
-class ForgetPasswordState with _$ForgetPasswordState {
-  const ForgetPasswordState({
-    // 이메일 필드 값
+class ForgotPasswordState with _$ForgotPasswordState {
+  const ForgotPasswordState({
     this.email = '',
-
-    // 유효성 검증 에러 메시지
     this.emailError,
-
-    // 비밀번호 재설정 요청 상태
     this.resetPasswordResult,
-
-    // 성공 메시지 (UI에 표시될 성공 알림)
-    this.successMessage,
   });
 
   final String email;
   final String? emailError;
   final AsyncValue<void>? resetPasswordResult;
-  final String? successMessage;
 }
