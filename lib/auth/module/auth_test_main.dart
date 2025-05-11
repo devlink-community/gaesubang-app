63f80929071d38920e572dd78d63cbb2dd4bd571
Kein Moon <successhelper2000@gmail.com>
2025-05-11 09:09:29 +0900

    feat(auth): 로그인 에러 수정, 회원가입 후 해당 계정으로 로그인 테스트 성공

--- lib/auth/module/auth_test_main.dart
+++ lib/auth/module/auth_test_main.dart
@@ -1,14 +1,18 @@
+import 'package:devlink_mobile_app/auth/data/data_source/user_storage.dart';
 import 'package:devlink_mobile_app/auth/module/auth_di.dart'; // mockLoginUseCaseProvider import
 import 'package:devlink_mobile_app/main.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
+
+  UserStorage.instance.initialize();
+
   runApp(
     ProviderScope(
-      overrides: [
-        loginUseCaseProvider.overrideWithProvider(mockLoginUseCaseProvider),
-      ],
+      // overrides: [
+      //   loginUseCaseProvider.overrideWithProvider(mockLoginUseCaseProvider),
+      // ],
       child: const MyApp(),
     ),
   );
