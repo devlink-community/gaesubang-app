fe7035ba6917dafbedfdfad822f5b2ecaf181a80
seonho99 <pandapad10@gmail.com>
2025-05-12 19:48:03 +0900

    feat: 요일 작성

---
+++ lib/attendance/presentation/component/weekday_label.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class WeekdayLabel extends StatelessWidget {
+  final String label;
+  const WeekdayLabel({super.key, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 40,
+      child: Text(
+        label,
+        textAlign: TextAlign.center,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w600,
+          color: Color(0xFFCFCFCF),
+        ),
+      ),
+    );
+  }
+}
