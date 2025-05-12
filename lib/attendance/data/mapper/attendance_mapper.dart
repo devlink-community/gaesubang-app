e7311618cc554be6a19604183cb2718cf7f7d362
seonho99 <pandapad10@gmail.com>
2025-05-12 19:11:13 +0900

    attendance mapper 작성

---
+++ lib/attendance/data/mapper/attendance_mapper.dart
@@ -0,0 +1,28 @@
+import '../../domain/model/attendance.dart';
+import '../dto/attendance_dto.dart';
+
+extension AttendanceDtoMapper on AttendanceDto {
+  Attendance toModel() => Attendance(
+    memberId: memberId,
+    groupId: groupId,
+    date: DateTime.parse(date),
+    time: time,
+  );
+}
+
+extension AttendanceDtoListMapper on List<AttendanceDto> {
+  List<Attendance> toModelList() => map((e) => e.toModel()).toList();
+}
+
+extension AttendanceModelMapper on Attendance {
+  AttendanceDto toDto() => AttendanceDto(
+    date: date.toIso8601String().split('T')[0],
+    memberId: memberId,
+    groupId: groupId,
+    time: time,
+  );
+}
+
+extension MapToAttendanceDto on Map<String, dynamic> {
+  AttendanceDto toAttendanceDto() => AttendanceDto.fromJson(this);
+}
