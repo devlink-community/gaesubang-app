18859854f9d276b316a5740a4b31814877562cd1
seonho99 <pandapad10@gmail.com>
2025-05-12 19:20:55 +0900

    출석부 상태 저장

---
+++ lib/attendance/presentation/attendance/attendance_state.dart
@@ -0,0 +1,29 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../domain/model/attendance.dart';
+import '../../domain/model/group.dart';
+
+part 'attendance_state.freezed.dart';
+
+@freezed
+class AttendanceState with _$AttendanceState {
+  const AttendanceState({
+    // 선택된 그룹 정보
+    this.selectedGroup,
+
+    // 현재 표시 중인 월 (캘린더 상단에 표시되는 년-월)
+    required this.displayedMonth,
+
+    // 캘린더에서 선택된 날짜
+    required this.selectedDate,
+
+    // 출석 정보 목록 (AsyncValue로 로딩/에러/데이터 상태 관리)
+    required this.attendanceList,
+  });
+
+  final Group? selectedGroup;
+  final DateTime displayedMonth;
+  final DateTime selectedDate;
+  final AsyncValue<List<Attendance>> attendanceList;
+}
