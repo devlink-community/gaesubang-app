3ac963aca76472ac6492b7f50734833d72a9a114
KangJiWon <doggodking@gmail.com>
2025-05-12 14:06:27 +0900

    fix(groupTimer): GroupTimerState에서 중복 MemberTimer 클래스 제거 및 domain 의존성 사용

--- lib/group/presentation/group_timer/group_timer_state.dart
+++ lib/group/presentation/group_timer/group_timer_state.dart
@@ -1,5 +1,5 @@
+import 'package:devlink_mobile_app/group/domain/model/member_timer.dart';
 import 'package:devlink_mobile_app/group/domain/model/timer_session.dart';
-import 'package:devlink_mobile_app/group/presentation/group_timer/components/member_timer_status.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -59,36 +59,4 @@ enum TimerStatus {
   running, // 실행 중
   paused, // 일시 정지
   completed, // 완료됨
-}
-
-// 멤버 타이머 정보를 담는 클래스
-class MemberTimer {
-  final String memberId;
-  final String memberName;
-  final String imageUrl;
-  final int elapsedSeconds;
-  final MemberTimerStatus status;
-
-  MemberTimer({
-    required this.memberId,
-    required this.memberName,
-    required this.imageUrl,
-    required this.elapsedSeconds,
-    required this.status,
-  });
-
-  // 시간 표시 문자열 (hh:mm:ss)
-  String get timeDisplay {
-    if (status == MemberTimerStatus.sleeping) return 'zzz';
-
-    final hours = elapsedSeconds ~/ 3600;
-    final minutes = (elapsedSeconds % 3600) ~/ 60;
-    final seconds = elapsedSeconds % 60;
-
-    if (hours > 0) {
-      return '${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
-    } else {
-      return '${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
-    }
-  }
 }
