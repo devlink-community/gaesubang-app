080a675fafe777704962914ef2db76c9c3bb86b4
seonho99 <pandapad10@gmail.com>
2025-05-12 18:18:10 +0900

    feat: dto 모델 작성

---
+++ lib/attendance/data/dto/attendance_dto.dart
@@ -0,0 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'attendance_dto.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class AttendanceDto {
+  const AttendanceDto({
+    required this.date,
+    required this.memberId,
+    required this.time,
+    required this.groupId,
+  });
+
+  final String date;
+  final String memberId;
+  final int time;
+  final String groupId;
+
+  factory AttendanceDto.fromJson(Map<String, dynamic> json) => _$AttendanceDtoFromJson(json);
+  Map<String, dynamic> toJson() => _$AttendanceDtoToJson(this);
+}
