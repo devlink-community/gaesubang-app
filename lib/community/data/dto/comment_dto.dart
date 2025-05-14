e036ce0613b0658013c7704ec1ac34944049650d
eastar-DS <dororo0104@naver.com>
2025-05-14 21:31:10 +0900

    refactor: CommentDto 수정

--- lib/community/data/dto/comment_dto.dart
+++ lib/community/data/dto/comment_dto.dart
@@ -2,13 +2,16 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 part 'comment_dto.freezed.dart';
 part 'comment_dto.g.dart';
+
 @freezed
 abstract class CommentDto with _$CommentDto {
   const factory CommentDto({
-    String?   boardId,
-    String?   memberId,
+    String? userId,
+    String? userName,
+    String? userProfileImage,
+    String? text,
     DateTime? createdAt,
-    String?   content,
+    int? likeCount,
   }) = _CommentDto;
 
   factory CommentDto.fromJson(Map<String, dynamic> json) =>
