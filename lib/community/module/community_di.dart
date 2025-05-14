392a6f8d585fae9d01054c6b2969398450816328
eastar-DS <dororo0104@naver.com>
2025-05-14 22:31:24 +0900

    refactor: 유즈케이스 di 추가

--- lib/community/module/community_di.dart
+++ lib/community/module/community_di.dart
@@ -1,8 +1,10 @@
+// lib/community/module/community_di.dart
 import 'package:devlink_mobile_app/community/data/data_source/mock_post_data_source_impl.dart';
 import 'package:devlink_mobile_app/community/domain/usecase/create_comment_use_case.dart';
 import 'package:devlink_mobile_app/community/domain/usecase/create_post_use_case.dart';
 import 'package:devlink_mobile_app/community/domain/usecase/fetch_comments_use_case.dart';
 import 'package:devlink_mobile_app/community/domain/usecase/fetch_post_detail_use_case.dart';
+import 'package:devlink_mobile_app/community/domain/usecase/search_posts_use_case.dart';  // 새로 추가
 import 'package:devlink_mobile_app/community/domain/usecase/toggle_bookmark_use_case.dart';
 import 'package:devlink_mobile_app/community/domain/usecase/toggle_like_use_case.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -15,16 +17,17 @@ import '../domain/usecase/load_post_list_use_case.dart';
 import '../domain/usecase/switch_tab_use_case.dart';
 
 part 'community_di.g.dart';
-//데이터소스
+
+// 데이터소스
 @riverpod
 PostDataSource postDataSource(Ref ref) => PostDataSourceImpl();
 
-//레포지토리
+// 레포지토리
 @riverpod
 PostRepository postRepository(Ref ref) =>
     PostRepositoryImpl(dataSource: ref.watch(postDataSourceProvider));
 
-//유즈케이스
+// 유즈케이스
 @riverpod
 LoadPostListUseCase loadPostListUseCase(Ref ref) =>
     LoadPostListUseCase(repo: ref.watch(postRepositoryProvider));
@@ -55,3 +58,7 @@ FetchCommentsUseCase fetchCommentsUseCase(Ref ref) =>
 @riverpod
 CreatePostUseCase createPostUseCase(Ref ref) =>
     CreatePostUseCase(repo: ref.watch(postRepositoryProvider));
+
+@riverpod
+SearchPostsUseCase searchPostsUseCase(Ref ref) =>
+    SearchPostsUseCase(repo: ref.watch(postRepositoryProvider));
