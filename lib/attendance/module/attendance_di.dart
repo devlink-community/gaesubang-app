0c07ab1d869c94baeee2cfa6a3ec2440f6735965
KangJiWon <doggodking@gmail.com>
2025-05-14 18:04:59 +0900

    refactor(router): 개별 도메인 라우터 파일 제거 및 중앙 라우터로 통합

--- lib/attendance/module/attendance_di.dart
+++ lib/attendance/module/attendance_di.dart
@@ -1,4 +1,3 @@
-import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -7,7 +6,6 @@ import '../data/data_source/mock_attendance_data_source_impl.dart';
 import '../data/repository/attendance_repository_impl.dart';
 import '../domain/repository/attendance_repository.dart';
 import '../domain/usecase/get_attendance_by_month_use_case.dart';
-import '../presentation/attendance/attendance_screen_root.dart';
 
 part 'attendance_di.g.dart';
 
@@ -29,24 +27,4 @@ AttendanceRepository attendanceRepository(Ref ref) {
 GetAttendancesByMonthUseCase getAttendancesByMonthUseCase(Ref ref) {
   final repository = ref.watch(attendanceRepositoryProvider);
   return GetAttendancesByMonthUseCase(repository);
-}
-
-@riverpod
-GoRouter attRouter(Ref ref){
-  final defaultGroup = MockGroupDataSourceImpl.getDefaultGroup();
-
-  return GoRouter(
-    // 앱 시작 시 바로 출석부 화면으로 이동
-    initialLocation: '/attendance',
-    routes: [
-      // 출석부 화면만 등록
-      GoRoute(
-        path: '/attendance',
-        builder: (context, state) {
-          // 항상 기본 그룹 사용
-          return AttendanceScreenRoot(group: defaultGroup);
-        },
-      ),
-    ],
-  );
 }
