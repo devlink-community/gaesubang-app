d1fbbbb2fecd90a39493f46a92dcaac9a87b0bff
KangJiWon <doggodking@gmail.com>
2025-05-16 17:22:36 +0900

    fix(groupList): 이미지 로드 및 스켈레톤 구현

---
+++ lib/group/presentation/group_list/components/group_list_skeleton.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+
+class GroupListSkeleton extends StatelessWidget {
+  final int itemCount;
+  final bool withAnimation;
+
+  const GroupListSkeleton({
+    super.key,
+    this.itemCount = 3,
+    this.withAnimation = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: itemCount,
+      itemBuilder: (context, index) {
+        return _buildSkeletonItem();
+      },
+    );
+  }
+
+  Widget _buildSkeletonItem() {
+    final Widget skeletonCard = Container(
+      margin: const EdgeInsets.only(bottom: 24),
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.05),
+            blurRadius: 15,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // 프로필 이미지 스켈레톤
+          Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.grey.shade200,
+            ),
+          ),
+          const SizedBox(width: 16),
+
+          // 그룹 정보 스켈레톤
+          Expanded(
+            child: SizedBox(
+              height: 100,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // 제목 스켈레톤
+                  Container(
+                    width: 150,
+                    height: 16,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.grey.shade200,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+
+                  // 방장 정보 스켈레톤
+                  Container(
+                    width: 100,
+                    height: 12,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: Colors.grey.shade200,
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+
+                  // 태그 스켈레톤
+                  Row(
+                    children: List.generate(
+                      3,
+                      (index) => Container(
+                        width: 50,
+                        height: 16,
+                        margin: const EdgeInsets.only(right: 8),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.grey.shade200,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  const Spacer(),
+
+                  // 하단 정보 스켈레톤
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 70,
+                        height: 12,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(6),
+                          color: Colors.grey.shade200,
+                        ),
+                      ),
+                      Container(
+                        width: 60,
+                        height: 20,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.grey.shade200,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    if (withAnimation) {
+      return _SkeletonAnimation(child: skeletonCard);
+    } else {
+      return skeletonCard;
+    }
+  }
+}
+
+/// 스켈레톤 UI에 애니메이션 효과를 적용하는 래퍼 위젯
+class _SkeletonAnimation extends StatefulWidget {
+  final Widget child;
+
+  const _SkeletonAnimation({required this.child});
+
+  @override
+  _SkeletonAnimationState createState() => _SkeletonAnimationState();
+}
+
+class _SkeletonAnimationState extends State<_SkeletonAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 애니메이션 컨트롤러 초기화
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    )..repeat(reverse: true);
+
+    // 애니메이션 커브 설정
+    _animation = Tween<double>(
+      begin: 0.4,
+      end: 0.8,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Opacity(opacity: _animation.value, child: widget.child);
+      },
+    );
+  }
+}
