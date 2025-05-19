d23d0541df1e61d23e8d80f92b44f73dbcb492e9
complete0415jiyoung <complete0415@naver.com>
2025-05-19 20:27:20 +0900

    utile: 안드로이드 아이디 변경

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.devlink_mobile_app"
+    namespace = "com.devlink.gaesubang"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -26,7 +26,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.devlink_mobile_app"
+        applicationId = "com.devlink.gaesubang"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
