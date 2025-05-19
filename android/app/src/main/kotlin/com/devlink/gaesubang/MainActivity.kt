f689ede8c62fa83482da3cb3eba19324b1dbf2a5
KangJiWon <doggodking@gmail.com>
2025-05-19 21:32:00 +0900

    refactor: 안드로이드 패키지명 변경
    - `com.example.devlink_mobile_app`에서 `com.devlink.gaesubang`으로 변경

--- android/app/src/main/kotlin/com/devlink/gaesubang/MainActivity.kt
+++ android/app/src/main/kotlin/com/devlink/gaesubang/MainActivity.kt
@@ -2,4 +2,8 @@ package com.devlink.gaesubang
 
 import io.flutter.embedding.android.FlutterActivity
 
+<<<<<<< HEAD
 class MainActivity : FlutterActivity() {}
+=======
+class MainActivity : FlutterActivity()
+>>>>>>> c1b6a7f0 (refactor: 안드로이드 패키지명 변경)
