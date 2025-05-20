bf3f8390833f92455da62400c7128abf548a2c8f
complete0415jiyoung <complete0415@naver.com>
2025-05-20 21:26:23 +0900

    feat: 그룹위치 usecase

---
+++ lib/map/domain/usecase/update_member_location_use_case.dart
@@ -0,0 +1,32 @@
+// lib/group/domain/usecase/update_member_location_use_case.dart
+import 'package:devlink_mobile_app/core/result/result.dart';
+import 'package:devlink_mobile_app/map/domain/repository/group_location_repository.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class UpdateMemberLocationUseCase {
+  final GroupLocationRepository _repository;
+
+  UpdateMemberLocationUseCase({required GroupLocationRepository repository})
+    : _repository = repository;
+
+  Future<AsyncValue<void>> execute(
+    String groupId,
+    String memberId,
+    double latitude,
+    double longitude,
+  ) async {
+    final result = await _repository.updateMemberLocation(
+      groupId,
+      memberId,
+      latitude,
+      longitude,
+    );
+
+    switch (result) {
+      case Success():
+        return const AsyncData(null);
+      case Error(failure: final failure):
+        return AsyncError(failure, failure.stackTrace ?? StackTrace.current);
+    }
+  }
+}
