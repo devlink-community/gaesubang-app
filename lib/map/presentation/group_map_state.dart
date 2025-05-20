ce7f1358d0332ebc31fde183b692fa4f7464b52d
complete0415jiyoung <complete0415@naver.com>
2025-05-20 21:27:32 +0900

    feat: 그룹위치 UI

---
+++ lib/map/presentation/group_map_state.dart
@@ -0,0 +1,66 @@
+// lib/map/presentation/group_map_state.dart
+import 'package:devlink_mobile_app/map/domain/model/group_member_location.dart';
+import 'package:devlink_mobile_app/map/domain/model/location.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+part 'group_map_state.freezed.dart';
+
+@freezed
+class GroupMapState with _$GroupMapState {
+  const GroupMapState({
+    // 현재 사용자 위치
+    this.currentLocation = const AsyncValue.loading(),
+
+    // 그룹 멤버 위치 목록
+    this.memberLocations = const AsyncValue.loading(),
+
+    // 선택된 멤버
+    this.selectedMember,
+
+    // 위치 추적 모드 활성화 여부
+    this.isTrackingMode = false,
+
+    // 맵 카메라 위치
+    this.cameraPosition,
+
+    // 맵 초기화 완료 여부
+    this.isMapInitialized = false,
+
+    // 위치 권한 상태
+    this.hasLocationPermission = false,
+
+    // 위치 서비스 활성화 여부
+    this.isLocationServiceEnabled = false,
+
+    // 에러 메시지
+    this.errorMessage,
+
+    // 검색 반경 (km)
+    this.searchRadius = 5.0,
+
+    // 데이터 로드 중 여부
+    this.isLoading = false,
+
+    // 그룹 ID
+    this.groupId = '',
+
+    // 그룹명
+    this.groupName = '',
+  });
+
+  final AsyncValue<Location> currentLocation;
+  final AsyncValue<List<GroupMemberLocation>> memberLocations;
+  final GroupMemberLocation? selectedMember;
+  final bool isTrackingMode;
+  final NCameraPosition? cameraPosition;
+  final bool isMapInitialized;
+  final bool hasLocationPermission;
+  final bool isLocationServiceEnabled;
+  final String? errorMessage;
+  final double searchRadius;
+  final bool isLoading;
+  final String groupId;
+  final String groupName;
+}
