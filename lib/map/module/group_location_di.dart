b57b8805f090a2761715414f37b964dee63a1b38
complete0415jiyoung <complete0415@naver.com>
2025-05-20 23:42:41 +0900

    feat: 그룹 멤버 위치 공유 mock 구현 완료

--- lib/map/module/group_location_di.dart
+++ lib/map/module/group_location_di.dart
@@ -1,6 +1,9 @@
 // lib/group/module/group_location_di.dart
+// ignore_for_file: deprecated_member_use_from_same_package
+
 import 'package:devlink_mobile_app/map/data/data_source/group_location_data_source_impl.dart';
 import 'package:devlink_mobile_app/map/data/data_source/group_loscation_data_source.dart';
+import 'package:devlink_mobile_app/map/data/data_source/mock_group_location_data_source.dart';
 import 'package:devlink_mobile_app/map/data/repository_impl/group_location_repository_impl.dart';
 import 'package:devlink_mobile_app/map/domain/repository/group_location_repository.dart';
 import 'package:devlink_mobile_app/map/domain/usecase/get_group_location_use_case.dart';
@@ -13,7 +16,8 @@ part 'group_location_di.g.dart';
 GroupLocationDataSource groupLocationDataSource(
   GroupLocationDataSourceRef ref,
 ) {
-  return GroupLocationDataSourceImpl();
+  // 실제 데이터 소스 대신 Mock 데이터 소스 사용
+  return MockGroupLocationDataSource();
 }
 
 @riverpod
