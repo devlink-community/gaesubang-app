1fa280ed446ad06eb9cc517de93a6cf991990cce
KangJiWon <doggodking@gmail.com>
2025-05-20 17:25:21 +0900

    feat(community): 게시글/댓글 조회 시 사용자 ID 파라미터 추가
    게시글 및 댓글 조회 관련 메서드에 `currentUserId` 파라미터를 추가하여, 현재 사용자에 따른 좋아요/북마크 상태 등을 처리할 수 있도록 개선했습니다.
    
    - `PostDataSource` 인터페이스의 다음 메서드들에 `currentUserId` 파라미터 추가:
        - `fetchPostList`
        - `fetchPostDetail`
        - `fetchComments`
        - `searchPosts`
    - `MockPostDataSourceImpl` 및 `PostFirebaseDataSource`의 해당 메서드 구현 변경:
        - `currentUserId` 파라미터를 받아 사용자의 좋아요/북마크 상태를 확인하는 로직에 활용
        - `PostFirebaseDataSource`에서 `FirebaseAuth.instance.currentUser?.uid` 대신 전달받은 `currentUserId` 사용

--- lib/community/data/data_source/post_data_source.dart
+++ lib/community/data/data_source/post_data_source.dart
@@ -4,17 +4,20 @@ import 'package:devlink_mobile_app/community/data/dto/post_dto.dart';
 
 abstract interface class PostDataSource {
   /* List */
-  Future<List<PostDto>> fetchPostList();
+  Future<List<PostDto>> fetchPostList({String? currentUserId});
 
   /* Detail */
-  Future<PostDto> fetchPostDetail(String postId);
+  Future<PostDto> fetchPostDetail(String postId, {String? currentUserId});
 
   /* Toggle - 사용자 정보 파라미터 추가 */
   Future<PostDto> toggleLike(String postId, String userId, String userName);
   Future<PostDto> toggleBookmark(String postId, String userId);
 
   /* Comment - 사용자 정보 파라미터 추가 */
-  Future<List<PostCommentDto>> fetchComments(String postId);
+  Future<List<PostCommentDto>> fetchComments(
+    String postId, {
+    String? currentUserId,
+  });
   Future<List<PostCommentDto>> createComment({
     required String postId,
     required String userId,
@@ -52,7 +55,7 @@ abstract interface class PostDataSource {
   );
 
   /* Search */
-  Future<List<PostDto>> searchPosts(String query);
+  Future<List<PostDto>> searchPosts(String query, {String? currentUserId});
 
   /* Create - 작성자 정보 파라미터 업데이트 */
   Future<String> createPost({
