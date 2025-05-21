d1dbccbb83788c9ab67275e1857915723778cd4d
Si Yujun <siuj0810@gmail.com>
2025-05-21 16:26:40 +0900

    feat: fcm_di 추가 완료

---
+++ lib/notification/module/fcm_di.dart
@@ -0,0 +1,11 @@
+import 'package:devlink_mobile_app/notification/service/fcm_service.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'fcm_di.g.dart';
+
+/// FCM 서비스 제공자
+@Riverpod(keepAlive: true)
+FCMService fcmService(Ref ref) {
+  return FCMService();
+}
