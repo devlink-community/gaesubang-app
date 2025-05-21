3fbfef0c57023f0dccfc80de8057087e5474cd63
Si Yujun <siuj0810@gmail.com>
2025-05-21 16:26:40 +0900

    feat: fcm_di 추가 완료

--- lib/notification/module/fcm_di.dart
+++ lib/notification/module/fcm_di.dart
@@ -1,5 +1,8 @@
 import 'package:devlink_mobile_app/notification/service/fcm_service.dart';
+<<<<<<< HEAD
 import 'package:devlink_mobile_app/notification/service/fcm_token_service.dart';
+=======
+>>>>>>> 0daf644b (feat: fcm_di 추가 완료)
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -10,9 +13,12 @@ part 'fcm_di.g.dart';
 FCMService fcmService(Ref ref) {
   return FCMService();
 }
+<<<<<<< HEAD
 
 /// FCM 토큰 서비스 제공자
 @Riverpod(keepAlive: true)
 FCMTokenService fcmTokenService(Ref ref) {
   return FCMTokenService();
 }
+=======
+>>>>>>> 0daf644b (feat: fcm_di 추가 완료)
