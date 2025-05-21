a81de7cdab078bb977dccf68755911c01b01993d
KangJiWon <doggodking@gmail.com>
2025-05-21 13:20:37 +0900

    refactor(group): Group 모델 및 관련 코드 수정
    - Group 모델의 createdBy 필드를 ownerId로 변경
    - Group 모델에 ownerNickname, ownerProfileImage 필드 추가
    - 그룹 목록 화면 및 아이템 컴포넌트에서 변경된 모델 반영
    - 그룹 목록 로직에서 현재 사용자 정보 직접 참조 제거 (isJoinedByCurrentUser 활용)
    - 일부 위젯에서 Color.withOpacity 대신 Color.withValues 사용
    - 스켈레톤 UI 컴포넌트 경로 수정 및 ErrorView 컴포넌트 활용

--- lib/group/presentation/group_list/group_list_state.dart
+++ lib/group/presentation/group_list/group_list_state.dart
@@ -2,22 +2,20 @@
 
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import '../../../auth/domain/model/member.dart';
+
 import '../../domain/model/group.dart';
 
 part 'group_list_state.freezed.dart';
 
 @freezed
 class GroupListState with _$GroupListState {
-  final AsyncValue<List<Group>> groupList;
-  final AsyncValue<Group?> selectedGroup;
-  final AsyncValue<void> joinGroupResult;
-  final Member? currentMember;
-
   const GroupListState({
     this.groupList = const AsyncValue.loading(),
     this.selectedGroup = const AsyncValue.data(null),
     this.joinGroupResult = const AsyncValue.data(null),
-    this.currentMember,
   });
+
+  final AsyncValue<List<Group>> groupList;
+  final AsyncValue<Group?> selectedGroup;
+  final AsyncValue<void> joinGroupResult;
 }
