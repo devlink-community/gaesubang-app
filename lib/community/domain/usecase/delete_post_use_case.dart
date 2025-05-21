fed0bfc7f941bc73a13b75d4bb467ffa7d8e4424
complete0415jiyoung <complete0415@naver.com>
2025-05-21 17:48:52 +0900

    feat: 게시글 수정 삭제 usecase 추가

---
+++ lib/community/domain/usecase/delete_post_use_case.dart
@@ -0,0 +1,20 @@
+// lib/community/domain/usecase/delete_post_use_case.dart
+import 'package:devlink_mobile_app/community/domain/repository/post_repository.dart';
+import 'package:devlink_mobile_app/core/result/result.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class DeletePostUseCase {
+  const DeletePostUseCase({required PostRepository repo}) : _repo = repo;
+  final PostRepository _repo;
+
+  Future<AsyncValue<bool>> execute(String postId) async {
+    final result = await _repo.deletePost(postId);
+
+    switch (result) {
+      case Success(:final data):
+        return AsyncData(data);
+      case Error(failure: final failure):
+        return AsyncError(failure, failure.stackTrace ?? StackTrace.current);
+    }
+  }
+}
