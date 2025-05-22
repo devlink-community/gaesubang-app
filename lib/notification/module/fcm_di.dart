7f4978434fbd18d26f4f6d21c17d434e7cde8700
Si Yujun <siuj0810@gmail.com>
2025-05-22 10:36:29 +0900

    fix: fcm di 토큰 서비스 제공 추가 완료

--- lib/notification/module/fcm_di.dart
+++ lib/notification/module/fcm_di.dart
@@ -1,4 +1,5 @@
 import 'package:devlink_mobile_app/notification/service/fcm_service.dart';
+import 'package:devlink_mobile_app/notification/service/user_token_service.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -8,4 +9,10 @@ part 'fcm_di.g.dart';
 @Riverpod(keepAlive: true)
 FCMService fcmService(Ref ref) {
   return FCMService();
+}
+
+/// FCM 토큰 서비스 제공자
+@Riverpod(keepAlive: true)
+FCMTokenService fcmTokenService(Ref ref) {
+  return FCMTokenService();
 }
