1bd03fe7c7045b8e830a3c6da0b49e396efbc9ce
Si Yujun <siuj0810@gmail.com>
2025-05-22 21:38:29 +0900

    fix: notification data source 조회 방법 변경

--- lib/notification/data/data_source/notification_data_source.dart
+++ lib/notification/data/data_source/notification_data_source.dart
@@ -4,12 +4,12 @@ abstract interface class NotificationDataSource {
   /// 알림 목록 조회
   Future<List<NotificationDto>> fetchNotifications(String userId);
 
-  /// 알림 읽음 처리
-  Future<bool> markAsRead(String notificationId);
+  /// 특정 알림 읽음 처리 - userId 추가
+  Future<bool> markAsRead(String userId, String notificationId);
 
   /// 모든 알림 읽음 처리
   Future<bool> markAllAsRead(String userId);
 
-  /// 알림 삭제
-  Future<bool> deleteNotification(String notificationId);
+  /// 특정 알림 삭제 - userId 추가
+  Future<bool> deleteNotification(String userId, String notificationId);
 }
