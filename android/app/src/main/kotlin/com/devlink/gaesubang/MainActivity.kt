b71e3920248174c8b06cb7062258dc4b31cbee1a
Si Yujun <siuj0810@gmail.com>
2025-05-22 19:32:32 +0900

    fix: rebase conflict 수정 완료

--- android/app/src/main/kotlin/com/devlink/gaesubang/MainActivity.kt
+++ android/app/src/main/kotlin/com/devlink/gaesubang/MainActivity.kt
@@ -2,8 +2,7 @@ package com.devlink.gaesubang
 
 import io.flutter.embedding.android.FlutterActivity
 
-<<<<<<< HEAD
-class MainActivity : FlutterActivity() {}
-=======
+
+
 class MainActivity : FlutterActivity()
->>>>>>> c1b6a7f0 (refactor: 안드로이드 패키지명 변경)
+
