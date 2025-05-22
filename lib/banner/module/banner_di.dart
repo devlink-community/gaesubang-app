716ee22551b31bf1f80cbaa6f0c3fdf582cf4f05
Kein Moon <successhelper2000@gmail.com>
2025-05-23 01:43:31 +0900

    feat(banner): 배너 의존성 주입 모듈 추가
    - banner_di.dart로 배너 기능 DI 설정 구성
    - Riverpod Provider 기반 계층별 의존성 주입 구현
    - Mock DataSource 기본 설정으로 테스트 환경 준비
    - 코드 생성 기반 타입 안전한 Provider 정의

---
+++ lib/banner/module/banner_di.dart
@@ -0,0 +1,24 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../data/data_source/banner_data_source.dart';
+import '../data/data_source/mock_banner_data_source_impl.dart';
+import '../data/repository_impl/banner_repository_impl.dart';
+import '../domain/repository/banner_repository.dart';
+import '../domain/usecase/get_active_banners_use_case.dart';
+
+part 'banner_di.g.dart';
+
+// DataSource Provider
+@riverpod
+BannerDataSource bannerDataSource(Ref ref) => MockBannerDataSourceImpl();
+
+// Repository Provider
+@riverpod
+BannerRepository bannerRepository(Ref ref) =>
+    BannerRepositoryImpl(dataSource: ref.watch(bannerDataSourceProvider));
+
+// UseCase Provider
+@riverpod
+GetActiveBannersUseCase getActiveBannersUseCase(Ref ref) =>
+    GetActiveBannersUseCase(repository: ref.watch(bannerRepositoryProvider));
