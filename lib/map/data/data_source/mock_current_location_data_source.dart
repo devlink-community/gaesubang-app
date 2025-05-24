b60838a03d9d6abec4876b6b4b06907d56dcf44a
eastar-DS <dororo0104@naver.com>
2025-05-24 17:34:36 +0900

    refactor(map): MockCurrentLocationDataSource의 print문을 AppLogger로 교체
    - 모든 kDebugMode 조건부 print() 호출을 AppLogger.debug()로 교체
    - 현재 위치 반환 및 위치 데이터 저장 관련 로그 개선
    - 태그를  'MockCurrentLocation'으로 설정하여 Mock 현재 위치 로그 필터링 개선
    - AppLogger import 추가
    - Mock 현재 위치 데이터 소스 디버깅 효율성 향상

--- lib/map/data/data_source/mock_current_location_data_source.dart
+++ lib/map/data/data_source/mock_current_location_data_source.dart
@@ -1,8 +1,8 @@
 // lib/map/data/data_source/mock_current_location_data_source.dart
+import 'package:devlink_mobile_app/core/utils/app_logger.dart';
 import 'package:devlink_mobile_app/map/data/data_source/map_data_source.dart';
 import 'package:devlink_mobile_app/map/data/dto/location_dto.dart';
 import 'package:devlink_mobile_app/map/data/dto/near_by_items_dto.dart';
-import 'package:flutter/foundation.dart';
 
 /// 현재 위치를 Mock으로 제공하는 DataSource 구현체
 class MockCurrentLocationDataSource implements MapDataSource {
@@ -16,9 +16,7 @@ class MockCurrentLocationDataSource implements MapDataSource {
 
   @override
   Future<LocationDto> fetchCurrentLocation() async {
-    if (kDebugMode) {
-      print('MockCurrentLocationDataSource: 서울역 위치 반환 (현재 사용자)');
-    }
+    AppLogger.debug('MockCurrentLocationDataSource: 서울역 위치 반환 (현재 사용자)', tag: 'MockCurrentLocation');
 
     // 지연 시간 시뮬레이션 (200ms)
     await Future.delayed(const Duration(milliseconds: 200));
@@ -43,9 +41,7 @@ class MockCurrentLocationDataSource implements MapDataSource {
   @override
   Future<void> saveLocationData(LocationDto location, String userId) async {
     // 이 메서드는 사용하지 않음
-    if (kDebugMode) {
-      print('MockCurrentLocationDataSource: 위치 데이터 저장 (아무 작업 안함)');
-    }
+    AppLogger.debug('MockCurrentLocationDataSource: 위치 데이터 저장 (아무 작업 안함)', tag: 'MockCurrentLocation');
   }
 
   @override
