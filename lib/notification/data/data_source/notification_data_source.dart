d8851682e3fe4f8754cc15d6513cdc7ae53df81b
Si Yujun <siuj0810@gmail.com>
2025-05-26 17:35:20 +0900

    fix: import 수정 완료

--- lib/notification/data/data_source/notification_data_source.dart
+++ lib/notification/data/data_source/notification_data_source.dart
@@ -1,4 +1,4 @@
-import 'package:devlink_mobile_app/notification/data/dto/notification_dto_old.dart';
+import 'package:devlink_mobile_app/notification/data/dto/notification_dto.dart';
 
 abstract interface class NotificationDataSource {
   /// 알림 목록 조회
